44d280dc776db9cad722091e1dd27119e00973fa
Henna Saajos <hennasofia98@gmail.com>
2026-09-12 17:11:36 +0300

    Random numeroiden arvonta
    Arvotaan kolme random numeroa 1-10 väliltä.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Random random = new Random();
+
+        int randomNumber1 = random.nextInt(10)+1;
+        int randomNumber2 = random.nextInt(10)+1;
+        int randomNumber3 = random.nextInt(10)+1;
+
+        System.out.println(randomNumber1 + " " + randomNumber2 + " " + randomNumber3);
     }
 }
